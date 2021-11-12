98e7596b7c835ed241a907a061c4520d3f0aa115
Zach Wilson <zwilson22495@gmail.com>
2021-11-12 11:45:46 -0800

    Built second test. Committing for red phase of testing

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,8 +2,14 @@ import calculate from '../src/js/calculator.js';
 
 describe ('calculate', () => {
   let age = 26;
-  let ageInDays = age * 365
+  let ageInDays = age * 365;
+
+
+
   test ('should show me my age in days instead of years', () => {
     expect(ageInDays).toEqual(9490);
   });
+
+  test ('should calculate my age on Mercury')
+  expect(mercuryAge).toEqual(107)
 });
