52191bf5b503415e668010db18d39b6b7cdcffbf
Zach Wilson <zwilson22495@gmail.com>
2021-11-12 11:48:16 -0800

    Returns completed second test for green phase of testing

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,13 +3,14 @@ import calculate from '../src/js/calculator.js';
 describe ('calculate', () => {
   let age = 26;
   let ageInDays = age * 365;
-
+  let mercuryAge = Math.floor(ageInDays / 88);
 
 
   test ('should show me my age in days instead of years', () => {
     expect(ageInDays).toEqual(9490);
   });
 
-  test ('should calculate my age on Mercury')
-  expect(mercuryAge).toEqual(107)
+  test ('should calculate my age on Mercury', () => {
+  expect(mercuryAge).toEqual(107);
+  });
 });
