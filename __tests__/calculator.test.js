5dfcd9ee44f7e569fa6c3198bb066b7941b8fc87
Zach Wilson <zwilson22495@gmail.com>
2021-11-12 14:13:14 -0800

    Third test committed in failing state

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -2,8 +2,11 @@ import calculate from '../src/js/calculator.js';
 
 describe ('calculate', () => {
   let age = 26;
-  let ageInDays = age * 365;
+  let ageInDays = age * 365 ;
   let mercuryAge = Math.floor(ageInDays / 88);
+  let venusAge = Math.floor(ageInDays / 225);
+  let marsAge = Math.floor(ageInDays / 687);
+  let jupiterAge = Math.floor(ageInDays / 4383);
 
 
   test ('should show me my age in days instead of years', () => {
@@ -11,6 +14,14 @@ describe ('calculate', () => {
   });
 
   test ('should calculate my age on Mercury', () => {
-  expect(mercuryAge).toEqual(107);
+    expect(mercuryAge).toEqual(107);
   });
+
+  test ('should calculate my age on Mercury', () => {
+    expect(mercuryAge).toEqual(107);
+    expect(venusAge).toEqual(42);
+    expect(marsAge).toEqual(13);
+    expect(jupiterAge).toEqual(6);
+  });
+
 });
